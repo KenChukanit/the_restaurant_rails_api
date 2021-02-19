886478132af7354f746950a896fdbed46392a303
KenChukanit <chonlasek.c@gmail.com>
2021-02-18 21:32:55 -0800

    update serializer

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
     has_secure_password
     has_many :reviews
+
+    def user_name
+        "#{username.capitalize}"
+    end
+
 end
--- app/serializers/food_serializer.rb
+++ app/serializers/food_serializer.rb
@@ -5,24 +5,30 @@ class FoodSerializer < ActiveModel::Serializer
               :ingredients,
               :calories,
               :img_url,
-              :sales_quantities
+              :sales_quantities,
+              :reviews
 
-    has_many  :reviews
-
-    class ReviewSerializer < ActiveModel::Serializer
+  has_many :reviews
+  class ReviewSerializer < ActiveModel::Serializer
+   
     attributes :id,
         :body,
         :rating,
         :created_at,
-        :updated_at
-    
-
-        belongs_to :user, key: :reviewer
-
-    class UserSerializer < ActiveModel::Serializer
-      attributes :id,
-                  :username
-    end
+        :updated_at,
+        :reviewer,
+  
       
+      def reviewer
+          object.user&.user_name
+      end
+    
+      belongs_to :user, key: :reviewer
+      class UserSerializer < ActiveModel::Serializer
+        attributes :id,
+                  :username,
+                  :isStaff
+      end
     end
+  
 end
