8ff23b1c8916c2925ed62af10333445da6562296
KenChukanit <chonlasek.c@gmail.com>
2021-03-23 14:06:51 -0700

    add react client

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins '127.0.0.1:5500', 'localhost:5500', 'https://rails-the-restaurant.herokuapp.com/' 
+      origins '127.0.0.1:5500', 'localhost:5500', 'https://rails-the-restaurant.herokuapp.com/','https://the-restaurant-client.herokuapp.com/' 
       resource(
         '/api/*', 
         headers: :any, 
