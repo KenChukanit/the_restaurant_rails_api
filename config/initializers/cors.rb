06c869659559e8ca524bea4ed4a806cf1a29b00c
KenChukanit <chonlasek.c@gmail.com>
2021-03-25 12:10:48 -0700

    fix cors1

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins '*' 
+      origins '*', 'localhost:3000', 'https://rails-the-restaurant.herokuapp.com','https://the-restaurant-client.herokuapp.com','https://project-1-the-restaurant.vercel.app' 
       resource(
         '/api/*', 
         headers: :any, 
