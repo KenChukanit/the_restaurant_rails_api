a3047eef8e734db98d979a7f0153114143447120
KenChukanit <chonlasek.c@gmail.com>
2021-03-25 12:15:45 -0700

    fix cors3

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins 'https://project-1-the-restaurant-3z2v1d2fi-kenchukanit.vercel.app/', 'localhost:3000', 'https://rails-the-restaurant.herokuapp.com','https://the-restaurant-client.herokuapp.com','https://project-1-the-restaurant.vercel.app' 
+      origins 'https://project-1-the-restaurant-3z2v1d2fi-kenchukanit.vercel.app', 'localhost:3000', 'https://rails-the-restaurant.herokuapp.com','https://the-restaurant-client.herokuapp.com','https://project-1-the-restaurant.vercel.app' 
       resource(
         '/api/*', 
         headers: :any, 
